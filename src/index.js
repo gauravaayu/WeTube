5e287b9bd1479fb6cc5a3c7230adcf94d9ccc8e0
Gaurav <mishrabhonu0143@gmail.com>
2023-06-07 16:04:06 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App/>);
